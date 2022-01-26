427a450f4556a3abc19212910d28b60097b11919
Shelton Suen <sheltonsuen@gmail.com>
2022-01-26 17:06:48 +0800

    group it with non breaking space

--- packages/wukong-parser/src/tokenizer/__tests__/index.test.ts
+++ packages/wukong-parser/src/tokenizer/__tests__/index.test.ts
@@ -13,36 +13,38 @@ const MOCK_DATA: Data = {
 }
 
 describe('skipSpace', () => {
-  it('should skip leading spaces', () => {
-    const data = {
-      ...MOCK_DATA,
-      input: `${String.fromCodePoint(32, 32, 32, 32)}token`,
-    }
+  describe('non breaking space', () => {
+    it('should skip leading spaces', () => {
+      const data = {
+        ...MOCK_DATA,
+        input: `${String.fromCodePoint(32, 32, 32, 32)}token`,
+      }
 
-    const result = skipSpace(data)
+      const result = skipSpace(data)
 
-    expect(result).toEqual({ ...data, state: { position: 4 } })
-  })
+      expect(result).toEqual({ ...data, state: { position: 4 } })
+    })
 
-  it('should skip leading non breaking spaces', () => {
-    const data = {
-      ...MOCK_DATA,
-      input: `${String.fromCodePoint(160, 160)}token`,
-    }
+    it('should skip leading non breaking spaces', () => {
+      const data = {
+        ...MOCK_DATA,
+        input: `${String.fromCodePoint(160, 160)}token`,
+      }
 
-    const result = skipSpace(data)
+      const result = skipSpace(data)
 
-    expect(result).toEqual({ ...data, state: { position: 2 } })
-  })
+      expect(result).toEqual({ ...data, state: { position: 2 } })
+    })
 
-  it('should skip leading tabs', () => {
-    const data = {
-      ...MOCK_DATA,
-      input: `${String.fromCodePoint(9, 9, 9)}token`,
-    }
+    it('should skip leading tabs', () => {
+      const data = {
+        ...MOCK_DATA,
+        input: `${String.fromCodePoint(9, 9, 9)}token`,
+      }
 
-    const result = skipSpace(data)
+      const result = skipSpace(data)
 
-    expect(result).toEqual({ ...data, state: { position: 3 } })
+      expect(result).toEqual({ ...data, state: { position: 3 } })
+    })
   })
 })
