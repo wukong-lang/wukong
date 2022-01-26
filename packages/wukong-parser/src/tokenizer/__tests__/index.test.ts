7e22c5cd116facac91e5fc8f995170a17f7b590e
Shelton Suen <sheltonsuen@gmail.com>
2022-01-26 16:27:00 +0800

    should skip leading spaces

---
+++ packages/wukong-parser/src/tokenizer/__tests__/index.test.ts
@@ -0,0 +1,33 @@
+import { Data, skipSpace } from '..'
+
+const MOCK_DATA: Data = {
+  state: { position: 0, line: 0 },
+  options: {
+    attachComment: false,
+    inModule: false,
+  },
+  input: '',
+  output: {
+    comments: [],
+  },
+}
+
+describe('skipSpace', () => {
+  it('should skip leading space', () => {
+    const data = { ...MOCK_DATA, input: '   token' }
+
+    const result = skipSpace(data)
+
+    expect(result).toEqual({
+      state: { position: 3, line: 0 },
+      options: {
+        attachComment: false,
+        inModule: false,
+      },
+      input: '   token',
+      output: {
+        comments: [],
+      },
+    })
+  })
+})
---
+++ packages/wukong-parser/src/tokenizer/index.ts
@@ -0,0 +1,49 @@
+import { Char } from '../strings/Char'
+
+interface State {
+  position: number
+  line: number
+}
+
+type Options = {
+  attachComment: boolean
+  inModule: boolean
+}
+
+export type Data = {
+  state: State
+  options: Options
+  input: string
+  output: {
+    comments: []
+  }
+}
+
+export const skipSpace = ({
+  input,
+  state,
+  options,
+  output: { comments, ...rest },
+}: Data): Data => {
+  let current = state.position
+  let char = input.charCodeAt(current)
+
+  while (current < input.length && char === Char.Space) {
+    current += 1
+
+    char = input.charCodeAt(current)
+  }
+
+  return {
+    input,
+    state: {
+      ...state,
+      position: current,
+    },
+    options,
+    output: {
+      ...rest,
+      comments,
+    },
+  }
+}
